5b4a4e6c3cff8934892f80ffe3481e0a8a145b2d
Cris <cristiansouza.agra@gmail.com>
2024-06-03 05:52:48 -0300

    Revert cors

--- src/main/java/com/example/ramengo/config/WebConfig.java
+++ src/main/java/com/example/ramengo/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.example.ramengo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.example.ramengo.interceptor.ApiKeyInterceptor;
@@ -18,15 +17,5 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(apiKeyInterceptor).addPathPatterns("/orders/**");
         registry.addInterceptor(apiKeyInterceptor).addPathPatterns("/broths/**");
         registry.addInterceptor(apiKeyInterceptor).addPathPatterns("/proteins/**");
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .exposedHeaders("x-api-key")
-                .allowCredentials(true);
     }
 }
